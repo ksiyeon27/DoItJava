d9ffa461e1b46a32a5492c5e89e93dddbeb147de
ksiyeon27 <siyeonk27@gmail.com>
2021-07-24 15:50:45 +0900

    Set interface

--- Collection Framework/src/collection/hashset/MemberHashSetTest.java
+++ Collection Framework/src/collection/hashset/MemberHashSetTest.java
@@ -1,6 +1,24 @@
+//갑자기 패키지가 안먹으면 자바 클린을 쓰자. 왕굳!
 package collection.hashset;
+import collection.Member;
 
 public class MemberHashSetTest {
-    
+    public static void main(String[] args){
+        MemberHashSet memberHashSet = new MemberHashSet();
+
+        Member memberLee = new Member(1001, "이지원");
+        Member memberSon = new Member(1002, "손민국");
+        Member memberPark = new Member(1003, "박서훤");
+
+
+        memberHashSet.addMember(memberLee);
+        memberHashSet.addMember(memberSon);
+        memberHashSet.addMember(memberPark);
+        memberHashSet.showAllMember();
+
+        Member memberHong = new Member(1003, "홍길동");
+        memberHashSet.addMember(memberHong);
+        memberHashSet.showAllMember();
+        
+    }
 }
-//이거부터 하기!
